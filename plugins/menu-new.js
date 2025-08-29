8e8eb7b36a4ae342a7e6b725d04e164e550889fd
kingmd786 <shahidkingmd874@gmail.com>
2025-08-29 16:52:05 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363403870276377@newsletter',
+                newsletterJid: '120363420842499241@newsletter',
                 newsletterName: config.OWNER_NAME,
                 serverMessageId: 143
             }
@@ -51,7 +51,7 @@ cmd({
                 return await conn.sendMessage(
                     from,
                     {
-                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/lom1ep.png' },
+                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/sxa61f.jpg' },
                         caption: menuCaption,
                         contextInfo: contextInfo
                     },
